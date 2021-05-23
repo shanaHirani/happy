5f01febd12bdd37cfa9e88ffb236150d4542609e
shana hirai <shana.hirnai@gamil.com>
2021-05-23 10:49:55 +1000

    test1

--- app/src/main/java/com/jetbrains/handson/mpp/myapplication/ui/MainActivity.kt
+++ app/src/main/java/com/jetbrains/handson/mpp/myapplication/ui/MainActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.jetbrains.handson.mpp.myapplication.R
 import com.jetbrains.handson.mpp.myapplication.databinding.ActivityMainBinding
-
+///test1////
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
